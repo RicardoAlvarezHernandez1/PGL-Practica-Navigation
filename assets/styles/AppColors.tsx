b40f6ec24e0b67891ba0faf2e59a2d934fb87fa7
rica07b <rica07b@gmail.com>
2023-11-28 09:09:50 +0000

    feat(AppColors.tsx): colores de la aplicación

---
+++ assets/styles/AppColors.tsx
@@ -0,0 +1,9 @@
+const AppColors = {
+  firstColor: "#E0FBFC",
+  secondColor: "lightblue",
+  thirdColor: "#9DB4C0",
+  fourthColor: "#5C6B73",
+  fifthColor: "#253237",
+};
+
+export default AppColors;
