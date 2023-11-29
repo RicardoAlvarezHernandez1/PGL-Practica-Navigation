3a9d689f8425634f3a093ba86c925b1124c4b4e2
rica07b <rica07b@gmail.com>
2023-11-29 08:58:52 +0000

    feat : implementacion del drawer en el componente App

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import Welcome from "./components/Welcome";
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { NavigationContainer, Theme } from "@react-navigation/native";
+import CustomDrawer from "./components/CustomDrawer";
+import appColors from "./assets/styles/AppColors";
 
 export default function App() {
+  const myTheme: Theme = {
+    dark: false,
+    colors: {
+      primary: "white",
+      background: appColors.secondColor,
+      card: appColors.firstColor,
+      text: "white",
+      border: "yellow",
+      notification: "purple",
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Welcome />
+    <View style={styles.appContainer}>
+      {/* <NavigationContainer theme={myTheme}> */}
+      <NavigationContainer>
+        <CustomDrawer></CustomDrawer>
+      </NavigationContainer>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
+    backgroundColor: "#fff",
   },
 });
