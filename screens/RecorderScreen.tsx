75b3f95f688b69e949264c04c2761885b9370d60
rica07b <rica07b@gmail.com>
2024-01-24 10:05:49 +0000

    feat : metodos para la grabacion y reproduccion del audio creados

--- screens/RecorderScreen.tsx
+++ screens/RecorderScreen.tsx
@@ -10,26 +10,47 @@ import React, { useRef } from "react";
 import { UserContext } from "../context/UserContext";
 import AppColors from "../assets/styles/AppColors";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { AVPlaybackStatus, Audio } from "expo-av";
+import { Audio } from "expo-av";
+import {
+  fadeAnimation,
+  getDurationFormatted,
+  getRecordingArray,
+  playRecordFile,
+  removeAllRecords,
+  saveURI,
+} from "../services/RecorderService";
+import { ScrollView } from "react-native-gesture-handler";
+
+type RecordFile = {
+  time: string;
+  uri: string;
+};
 
 const RecorderScreen = () => {
   const { user, setUserName } = React.useContext(UserContext);
   const [isRecording, setisRecording] = React.useState(false);
   const [fadeAnim, setfadeAnim] = React.useState(new Animated.Value(0));
   const [recording, setRecording] = React.useState<Audio.Recording>();
-  const [recordings, setRecordings] = React.useState<
-    Array<{
-      sound: Audio.Sound;
-      duration: string;
-      file: string | null;
-    }>
-  >([]);
+  const [recordings, setRecordings] = React.useState<Array<RecordFile>>([]);
   const [message, setMessage] = React.useState("");
+
+  React.useEffect(() => {
+    const recordingsFunction = async () => {
+      const data = await getRecordingArray();
+      if (data != null) {
+        setRecordings(JSON.parse(data));
+      } else {
+        console.log("No hay grabaciones");
+      }
+    };
+    recordingsFunction();
+  }, []);
 
   async function startRecording() {
     try {
       const permission = await Audio.requestPermissionsAsync();
       if (permission.status === "granted") {
+        setisRecording(!isRecording);
         await Audio.setAudioModeAsync({
           allowsRecordingIOS: true,
           playsInSilentModeIOS: true,
@@ -47,62 +68,68 @@ const RecorderScreen = () => {
     }
   }
 
-  function getDurationFormatted(millis: number) {
-    const minutes = millis / 1000 / 60;
-    const minutesDisplay = Math.floor(minutes);
-    const seconds = Math.round((minutes - minutesDisplay) * 60);
-    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
-    return `${minutesDisplay}:${secondsDisplay}`;
-  }
-
   async function stopRecording() {
-    if (recording) {
-      try {
-        setRecording(undefined);
-        await recording.stopAndUnloadAsync();
-        let updatedRecordings = [...recordings];
-        const { sound, status } = await recording.createNewLoadedSoundAsync();
-        updatedRecordings.push({
-          sound: sound,
-          duration: getDurationFormatted(
-            (await recording.getStatusAsync()).durationMillis
-          ),
-          file: recording.getURI(),
-        });
-        setRecordings(updatedRecordings);
-      } catch (err) {
-        console.error("Failed to stop recording", err);
-        setRecording(undefined);
-      }
-    } else {
-      console.log("Error on stop recording.");
+    setRecording(undefined);
+
+    if (recording !== undefined) {
+      await recording.stopAndUnloadAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+      });
+      let updatedRecordings = [...recordings];
+      const recordTime: number = (await recording.getStatusAsync())
+        .durationMillis;
+
+      const uriRecord = recording.getURI();
+      updatedRecordings.push({
+        time: getDurationFormatted(recordTime),
+        uri: uriRecord ?? "",
+      });
+
+      setRecordings(updatedRecordings);
+      saveURI(JSON.stringify(updatedRecordings));
+      setisRecording(!isRecording);
     }
   }
 
-  const fadeAnimation = () => {
-    {
-      if (!isRecording) {
-        Animated.sequence([
-          Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true,
-          }),
-          Animated.timing(fadeAnim, {
-            toValue: 0,
-            duration: 1000,
-            useNativeDriver: true,
-          }),
-        ]).start(() => {
-          fadeAnimation();
-        });
-      }
-    }
+  const removeRecord = (item: RecordFile) => {
+    const newRecordArray: RecordFile[] = recordings.filter(
+      (data) => data != item
+    );
+    saveURI(JSON.stringify(newRecordArray));
+    setRecordings(newRecordArray);
   };
 
-  const OnclickButton = () => {
-    setisRecording(!isRecording);
+  const removeRecordsList = () => {
+    removeAllRecords();
+    setRecordings([]);
   };
+
+  function getRecordingLines() {
+    if (recordings.length == 0) {
+      return (
+        <View style={styles.noRecordings}>
+          <Text>No recordings to show</Text>
+        </View>
+      );
+    } else {
+      return recordings.map((recordingLine, index) => {
+        return (
+          <View key={index} style={styles.individualRecordContainer}>
+            <Text>
+              Recording {index + 1} - {recordingLine.time}
+            </Text>
+            <TouchableOpacity onPress={() => playRecordFile(recordingLine)}>
+              <Ionicons name={"play-outline"} size={20} color={"black"} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => removeRecord(recordingLine)}>
+              <Ionicons name={"trash-outline"} size={20} color={"black"} />
+            </TouchableOpacity>
+          </View>
+        );
+      });
+    }
+  }
 
   return (
     <View style={styles.mainContainer}>
@@ -119,25 +146,63 @@ const RecorderScreen = () => {
           </Text>
         </View>
         {!isRecording ? (
-          <View style={{ ...styles.boxShadow, ...styles.recordContainer }}>
-            <TouchableOpacity
-              onPress={() => OnclickButton()}
-              onPressIn={() => fadeAnimation()}
-            >
-              <Ionicons name={"mic-circle-outline"} size={50} color={"black"} />
-            </TouchableOpacity>
-          </View>
+          <>
+            <View style={{ ...styles.boxShadow, ...styles.recordContainer }}>
+              <TouchableOpacity
+                onPress={() => {
+                  startRecording(), fadeAnimation(fadeAnim);
+                }}
+              >
+                <Ionicons
+                  name={"mic-circle-outline"}
+                  size={50}
+                  color={"black"}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.recordsContainer}>
+              <View style={styles.recordsTitleContainer}>
+                <Text style={styles.descriptionRecords}>RECORDS</Text>
+                <TouchableOpacity onPress={() => removeRecordsList()}>
+                  <Text style={{ marginTop: 15, color: "red" }}>
+                    Delete All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <ScrollView style={styles.scroll}>
+                {getRecordingLines()}
+              </ScrollView>
+            </View>
+          </>
         ) : (
-          <View style={{ ...styles.boxShadow, ...styles.isRecordingContainer }}>
-            <TouchableOpacity onPress={() => OnclickButton()}>
-              <Ionicons name={"stop-outline"} size={40} color={"red"} />
-            </TouchableOpacity>
-            <Animated.Text
-              style={{ ...styles.recordingText, opacity: fadeAnim }}
+          <>
+            <View
+              style={{ ...styles.boxShadow, ...styles.isRecordingContainer }}
             >
-              Recording...{" "}
-            </Animated.Text>
-          </View>
+              <TouchableOpacity
+                style={{ flexDirection: "row", alignItems: "center" }}
+                onPress={() => stopRecording()}
+              >
+                <Ionicons name={"stop-outline"} size={40} color={"red"} />
+                <Animated.Text
+                  style={{ ...styles.recordingText, opacity: fadeAnim }}
+                >
+                  Recording...{" "}
+                </Animated.Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.recordsContainer}>
+              <View style={styles.recordsTitleContainer}>
+                <Text style={styles.descriptionRecords}>RECORDS</Text>
+                <TouchableOpacity onPress={() => removeRecordsList()}>
+                  <Text style={{ marginTop: 5, color: "red" }}>Delete All</Text>
+                </TouchableOpacity>
+              </View>
+              <ScrollView style={styles.scroll}>
+                {getRecordingLines()}
+              </ScrollView>
+            </View>
+          </>
         )}
       </ImageBackground>
     </View>
@@ -174,15 +239,34 @@ const styles = StyleSheet.create({
     padding: 6,
     marginTop: 10,
   },
+  scroll: {
+    width: 200,
+  },
   recordingText: {
     fontSize: 20,
     fontWeight: "400",
     color: "red",
   },
+  recordsTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+  },
+  noRecordings: {
+    alignSelf: "center",
+    marginTop: 10,
+  },
   description: {
     fontSize: 15,
     fontWeight: "400",
     textAlign: "center",
+  },
+  descriptionRecords: {
+    fontSize: 20,
+    fontWeight: "500",
+    textAlign: "center",
+    padding: 10,
   },
   recordContainer: {
     width: 80,
@@ -191,6 +275,25 @@ const styles = StyleSheet.create({
     backgroundColor: AppColors.firstColor,
     borderRadius: 60,
     marginTop: 10,
+  },
+  individualRecordContainer: {
+    width: 200,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: AppColors.firstColor,
+    borderRadius: 30,
+    marginTop: 8,
+    padding: 5,
+  },
+  recordsContainer: {
+    width: 220,
+    height: 260,
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: AppColors.firstColor,
+    borderRadius: 30,
+    marginTop: 8,
+    padding: 10,
   },
   descriptionContainer: {
     width: 300,
@@ -208,20 +311,6 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     paddingBottom: 20,
     marginVertical: 10,
-  },
-  buttonContent: {
-    color: "black",
-    fontWeight: "700",
-  },
-  touchable: {
-    marginTop: 15,
-    marginBottom: 15,
-    borderColor: "white",
-    borderWidth: 2,
-    borderRadius: 15,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    backgroundColor: AppColors.secondColor,
   },
   boxShadow: {
     shadowColor: "black",
